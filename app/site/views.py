bedbfb79ead91743bbf39769d7753b82d017efb2
Roberto Preste <roberto.preste@gmail.com>
2018-10-30 15:05:34 +0100

    Add basic Query page

--- app/site/views.py
+++ app/site/views.py
@@ -4,6 +4,7 @@
 # import requests
 # import json
 from quart import Blueprint, render_template
+from app.static import dbdata
 # from flask import Blueprint, render_template, flash, redirect, session, url_for, request, g, jsonify, send_file
 # from werkzeug.urls import url_parse
 
@@ -14,7 +15,6 @@ www = Blueprint("site", __name__)
 # from config import ADMINS
 # from .forms import LoginForm, RegistrationForm
 # from .models import User
-from app.static import dbdata
 
 
 # Home Page
@@ -23,20 +23,26 @@ from app.static import dbdata
 @www.route("/", methods=["GET"])
 async def index():
     return await render_template("index.html",
-                           title="Home",
-                           latest_update=dbdata.latest_update)
+                                 title="Home",
+                                 latest_update=dbdata.latest_update)
 
 
 @www.route("/about", methods=["GET"])
 async def about():
     return await render_template("about.html",
-                           title="About")
+                                 title="About")
 
 
 @www.route("/contacts", methods=["GET"])
 async def contacts():
     return await render_template("contacts.html",
-                           title="Contacts")
+                                 title="Contacts")
+
+
+@www.route("/query", methods=["GET", "POST"])
+async def query():
+    return await render_template("query.html",
+                                 title="Query")
 
 
 @www.errorhandler(404)
